0a683c7df7ff5305856b773658fc11965b569fc2
Manas Mishra <manasissotechy@gmail.com>
2025-07-30 15:16:59 +0530

    custom 404 edited

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <title>404 - Page Not Found</title>
-    <link rel="stylesheet" href="{{ asset('assets/css/style-404.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/assets/css/style-404.css') }}">
 </head>
 <body>
     <div class="container">
-        <img src="{{ asset('assets/img/e64fb3cb-96c6-4af3-b35f-67435dd5c05c.jpg') }}" alt="404 Error Image" class="error-image">
+        <img src="{{ asset('public/assets/img/e64fb3cb-96c6-4af3-b35f-67435dd5c05c.jpg') }}" alt="404 Error Image" class="error-image">
         <a href="/" class="home-btn">Home Page</a>
     </div>
 </body>
